df45ba8eb3f4184500ff67a31cc20bf289230499
Carolina Ferreira <carolbeatles97@gmail.com>
2023-01-05 19:42:18 +0000

    Correções

---
+++ Tp_Pweb_22_23/Tp_Pweb_22_23/Migrations/20230104231257_versao4Jan.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tp_Pweb_22_23.Migrations
+{
+    public partial class versao4Jan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
