a65660a582bd0fe49493b63befb6c67b63d2000b
Carolina Ferreira <carolbeatles97@gmail.com>
2023-01-07 03:47:03 +0000

    prettier

--- Tp_Pweb_22_23/Tp_Pweb_22_23/Models/EstadoVeiculo.cs
+++ Tp_Pweb_22_23/Tp_Pweb_22_23/Models/EstadoVeiculo.cs
@@ -9,6 +9,7 @@ namespace Tp_Pweb_22_23.Models
         public ESTADO ESTADO { get; set; }
         public int NumeroKm { get; set; }
         public bool Danos { get; set; }
+        [Display(Name = "Observações ")]
         public string? Observacoes { get; set; }
         public string? FuncionarioId { get; set; }
         public ApplicationUser? Funcionario { get; set; }
